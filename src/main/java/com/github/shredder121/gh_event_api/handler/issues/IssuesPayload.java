775befa2a187ace7f5ec74dce4e6d41f8fd7b4e8
Ruben Dijkstra <rubendijkstra123@gmail.com>
2016-01-08 16:46:33 +0100

    Add handler support for the 'issues' event

---
+++ src/main/java/com/github/shredder121/gh_event_api/handler/issues/IssuesPayload.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2016 Shredder121.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.shredder121.gh_event_api.handler.issues;
+
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.github.shredder121.gh_event_api.model.Issue;
+import com.github.shredder121.gh_event_api.model.Label;
+import com.github.shredder121.gh_event_api.model.User;
+
+/**
+ * The payload passed when an {@code issues} event is received.
+ *
+ * @author Shredder121
+ */
+@lombok.Value
+public class IssuesPayload {
+
+    /**
+     * The action that was performed.
+     */
+    @NotNull String action;
+
+    /**
+     * The issue itself.
+     */
+    @NotNull Issue issue;
+
+    /**
+     * The optional user who was assigned or unassigned from the issue.
+     */
+    User assignee;
+
+    /**
+     * The optional label that was added or removed from the issue.
+     */
+    Label label;
+
+    @JsonCreator
+    IssuesPayload(
+            @JsonProperty("action") String action,
+            @JsonProperty("issue") Issue issue,
+            @JsonProperty("assignee") User assignee,
+            @JsonProperty("label") Label label) {
+
+        this.action = action;
+        this.issue = issue;
+        this.assignee = assignee;
+        this.label = label;
+    }
+}
---
+++ src/test/java/com/github/shredder121/gh_event_api/handler/issues/IssuesHandlerTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2016 Shredder121.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.shredder121.gh_event_api.handler.issues;
+
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import com.github.shredder121.gh_event_api.GHEventApiServer;
+import com.github.shredder121.gh_event_api.handler.AbstractHandlerTest;
+
+@SpringApplicationConfiguration(classes = {IssuesHandlerTest.class, GHEventApiServer.class})
+public class IssuesHandlerTest extends AbstractHandlerTest {
+
+    public IssuesHandlerTest() {
+        super("issues", "7eb9a55d933dc1bf5a96e42d5b46a6d71de5562e");
+    }
+
+    @Bean
+    private TestHandler handlerBean() {
+        return new TestHandler();
+    }
+}

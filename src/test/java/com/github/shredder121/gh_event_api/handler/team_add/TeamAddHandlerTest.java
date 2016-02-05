f9f53654eb5199e552fee7d0b91869bc30143095
Ruben Dijkstra <rubendijkstra123@gmail.com>
2016-02-06 00:11:32 +0100

    Add support for the 'team_add' event

---
+++ src/test/java/com/github/shredder121/gh_event_api/handler/team_add/TeamAddHandlerTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2016 Shredder121.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.shredder121.gh_event_api.handler.team_add;
+
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import com.github.shredder121.gh_event_api.GHEventApiServer;
+import com.github.shredder121.gh_event_api.handler.AbstractHandlerTest;
+
+@SpringApplicationConfiguration(classes = {TeamAddHandlerTest.class, GHEventApiServer.class})
+public class TeamAddHandlerTest extends AbstractHandlerTest {
+
+    public TeamAddHandlerTest() {
+        super("team_add", "8eedb84b5dc45b5494bf8822682f3c1a07e355df");
+    }
+
+    @Bean
+    private TestHandler handlerBean() {
+        return new TestHandler();
+    }
+}

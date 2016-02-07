37ab622d9df3273ba00a34c3856b5512e789b906
Ruben Dijkstra <rubendijkstra123@gmail.com>
2016-02-07 15:47:57 +0100

    Add support for the 'member' event

---
+++ src/main/java/com/github/shredder121/gh_event_api/handler/member/MemberPayload.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2016 Shredder121.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.shredder121.gh_event_api.handler.member;
+
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy.LowerCaseWithUnderscoresStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.github.shredder121.gh_event_api.model.Repository;
+import com.github.shredder121.gh_event_api.model.User;
+import com.github.shredder121.gh_event_api.model.json.PropertyBasedJsonCreator;
+
+/**
+ * The payload passed when a {@code member} event is received.
+ *
+ * @author Shredder121
+ */
+@lombok.Value
+@JsonNaming(LowerCaseWithUnderscoresStrategy.class)
+@lombok.AllArgsConstructor(
+        access = lombok.AccessLevel.PACKAGE,
+        onConstructor = @__(@PropertyBasedJsonCreator)
+)
+public class MemberPayload {
+
+    /**
+     * The action that was performed.
+     * Currently, can only be 'added'.
+     */
+    @NotNull String action;
+
+    /**
+     * The user that was added.
+     */
+    @NotNull User member;
+
+    /**
+     * The repository the user was added to.
+     */
+    @NotNull Repository repository;
+
+    /**
+     * The user that triggered the action.
+     */
+    @NotNull User sender;
+}
---
+++ src/test/java/com/github/shredder121/gh_event_api/handler/member/TestHandler.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2016 Shredder121.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.shredder121.gh_event_api.handler.member;
+
+import static com.github.shredder121.gh_event_api.testutil.HamcrestHelpers.property;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+import com.github.shredder121.gh_event_api.handler.AbstractTestHandlerBean;
+import com.github.shredder121.gh_event_api.model.Repository;
+import com.github.shredder121.gh_event_api.model.User;
+
+class TestHandler extends AbstractTestHandlerBean implements MemberHandler {
+
+    @Override
+    public void handle(MemberPayload payload) {
+        errorCollector.checkThat(payload.getAction(), is("added"));
+
+        errorCollector.checkThat(payload.getMember(), allOf(
+                property(User::getId, is(583231)),
+                property(User::getLogin, is("octocat")),
+                property(User::getUrl, is("https://api.github.com/users/octocat"))
+        ));
+
+        errorCollector.checkThat(payload.getRepository(), allOf(
+                property(Repository::getName, is("public-repo")),
+                property(Repository::getFullName, is("baxterthehacker/public-repo"))
+        ));
+
+        errorCollector.checkThat(payload.getSender(), allOf(
+                property(User::getId, is(6752317)),
+                property(User::getLogin, is("baxterthehacker")),
+                property(User::getUrl, is("https://api.github.com/users/baxterthehacker"))
+        ));
+
+        countDownLatch.countDown();
+    }
+}

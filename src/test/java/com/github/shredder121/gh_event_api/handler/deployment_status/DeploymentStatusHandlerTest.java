01ae14bc23513d78229b2e1540e0f1e6cfb80a67
Ruben Dijkstra <rubendijkstra123@gmail.com>
2016-02-15 21:53:31 +0100

    Add support for the 'deployment_status' event

---
+++ src/test/java/com/github/shredder121/gh_event_api/handler/deployment_status/DeploymentStatusHandlerTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2016 Shredder121.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.shredder121.gh_event_api.handler.deployment_status;
+
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import com.github.shredder121.gh_event_api.GHEventApiServer;
+import com.github.shredder121.gh_event_api.handler.AbstractHandlerTest;
+
+@SpringApplicationConfiguration(classes = {DeploymentStatusHandlerTest.class, GHEventApiServer.class})
+public class DeploymentStatusHandlerTest extends AbstractHandlerTest {
+
+    public DeploymentStatusHandlerTest() {
+        super("deployment_status", "51c81ec5334b8e244783a3661ae1f9be12fb009a");
+    }
+
+    @Bean
+    private TestHandler handlerBean() {
+        return new TestHandler();
+    }
+}
